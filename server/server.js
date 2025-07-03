545d2ed1b0185ee748ab9b51fb0b4c2242417da8
Tanish <tanishpashte.24@gmail.com>
2025-07-03 16:07:22 +0530

    feat: Setup MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,31 +1,45 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const path = require('path'); // To serve static files from client
+const path = require('path'); 
+const { default: mongoose } = require('mongoose');
+const cors = require('cors');
 
-dotenv.config(); // Load environment variables from .env file
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000; // Use port from .env or default to 5000
+const PORT = process.env.PORT || 5000; 
 
-// Middleware to parse JSON bodies
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.error("MongoDB connection failed: ", err.message);
+        process.exit(1);
+    }
+};
+
+connectDB();
+
+// Middleware
 app.use(express.json());
+app.use(cors());
 
-// Serve static files from the client build directory if in production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+// // Serve static files from the client build directory if in production
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
-    });
-} else {
-    // In development, just log API requests
-    console.log('Server running in development mode.');
-}
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+//     });
+// } else {
+//     console.log('Server running in development mode.');
+// }
 
 
 // Basic API route
-app.get('/api/message', (req, res) => {
-    res.json({ message: 'Hello from the server!' });
+app.get('/', (req, res) => {
+  res.send('Hello from the MERN server!');
 });
 
 // Start the server
