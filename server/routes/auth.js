e6190206672252c244cd5920bc2ad819365bee4c
Tanish <tanishpashte.24@gmail.com>
2025-07-04 00:26:32 +0530

    feat(auth): Implement user login endpoint

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -60,7 +60,7 @@ router.post('/register', async (req, res) => {
 
 
     } catch (error) {
-        console.error(err.message);
+        console.error(error.message);
         res.status(500).send('Server Error');
     }
 });
@@ -68,8 +68,47 @@ router.post('/register', async (req, res) => {
 // @route POST api/auth/login
 // @desc Authenticate user and get token
 // @access Public
-router.post('/login', (req, res) => {
-    res.send('Login route');
-})
+router.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        if(!email || !password){
+            return res.status(400).json({msg:'Please enter all fields'});
+        }
+
+        // check if user exists
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({msg: 'Invalid Credentials'});
+        }
+
+        // check password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch){
+            return res.status(400).json({msg: 'Invalid Credentials'});
+        }
+
+        // generate and return JWT
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload, 
+            process.env.JWT_SECRET,
+            {expiresIn: '3h'},
+            (err, token) => {
+                if (err) throw err;
+                res.json({token});
+            }
+        );
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({msg: 'Server error'});
+    }
+});
 
 module.exports = router;
